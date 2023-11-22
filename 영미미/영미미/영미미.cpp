7f82cd133f118a40bf63b5dc9dd886a972dd24dc
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2023-11-22 17:32:56 +0900

    Add files via upload

---
+++ 영미미/영미미/영미미.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+// 정수의 family
+
+// char : 1
+// short : 2
+// int : 4
+// __int64 (long long) : 8
+
+int hp;
+int maxHp;
+
+int main()
+{
+    maxHp = 1000;
+    hp = maxHp;
+
+    cout << hp;
+}
